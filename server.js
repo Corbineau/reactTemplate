6ef8c91b8806da1069b39d2b93bc75f243840781
Rabbit <Corbineau@users.noreply.github.com>
2019-12-09 18:00:48 -0500

    added genericised API routes

--- server.js
+++ server.js
@@ -31,10 +31,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "/client/build"));
 }
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "client/build/index.html"));
-// });
-
 app.use(routes);
 
 // Start the API server
