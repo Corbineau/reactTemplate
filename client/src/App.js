63c01444e468858b971a1cb913c21cdc92c8c615
corbineau <lochinvar@gmail.com>
2019-12-11 23:47:46 -0500

    adding controllers

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Router from "./Router";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
